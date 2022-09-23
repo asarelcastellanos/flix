e61ac71eaaf3b36ccd53b49b446c16b6e325d674
Asarel Castellanos <asarelc@gmail.com>
2022-09-22 18:05:44 -0700

    you can now click on cell and get more information on movie

---
+++ flix/MovieDetailsViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MovieDetailsViewController.swift
+//  flix
+//
+//  Created by Asarel Castellanos on 9/22/22.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieDetailsViewController: UIViewController {
+    
+    var movie: [String:Any]!
+
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit();
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit();
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185";
+        let posterPath = movie["poster_path"] as! String;
+        let posterUrl = URL(string: baseUrl + posterPath);
+        
+        posterView.af.setImage(withURL: posterUrl!);
+        
+        let backdropPath = movie["backdrop_path"] as! String;
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath);
+        
+        backdropView.af.setImage(withURL: backdropUrl!);
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- flix/ViewController.swift
+++ flix/ViewController.swift
@@ -11,6 +11,7 @@ import AlamofireImage
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var tableView: UITableView!
+    
     var movies = [[String:Any]]()
     
     override func viewDidLoad() {
@@ -52,16 +53,32 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let title = movie["title"] as! String
         let synopsis = movie["overview"] as! String
         
-        cell.titleLabel?.text = title
-        cell.synopsisLabel?.text = synopsis
+        cell.titleLabel?.text = title;
+        cell.synopsisLabel?.text = synopsis;
         
-        let baseUrl = "https://image.tmdb.org/t/p/w185"
-        let posterPath = movie["poster_path"] as! String
-        let posterUrl = URL(string: baseUrl + posterPath)
+        let baseUrl = "https://image.tmdb.org/t/p/w185";
+        let posterPath = movie["poster_path"] as! String;
+        let posterUrl = URL(string: baseUrl + posterPath);
         
-        cell.posterView.af.setImage(withURL: posterUrl!)
+        cell.posterView.af.setImage(withURL: posterUrl!);
         
         return cell;
+
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("Loading up the details screen");
+        
+        // Find the selected movie
+        let cell = sender as! UITableViewCell;
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row];
+        
+        // Pass the selected movie to the details view controller
+        let detailViewController = segue.destination as! MovieDetailsViewController;
+        detailViewController.movie = movie;
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
 }
